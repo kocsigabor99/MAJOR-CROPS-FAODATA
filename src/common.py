5acdf8e9f5ddbd1dd1a00e5dc54012df42f84148
Erwin Rossen <erwin.rossen@gmail.com>
2024-07-20 13:36:54 +0200

    [WIP] UN Population API

--- src/common.py
+++ src/common.py
@@ -8,6 +8,6 @@ JSON = None | bool | int | float | str | list['JSON'] | dict[str, 'JSON']
 
 
 def get_secret(secret_key: str) -> JSON:
-    with open(os.path.join(DATA_DIR, 'secrets.json')) as f:
+    with open(os.path.join(SRC_DIR, 'secrets.json')) as f:
         secrets = json.load(f)
     return secrets[secret_key]
